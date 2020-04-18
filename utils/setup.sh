b6135e6359a31764d06af2c67ee93f84ca2c9c1c
Pietro <pietro.govoni@cern.ch>
2020-04-18 14:57:42 +0200

    LCG environment source file

---
+++ utils/setup.sh
@@ -0,0 +1,4 @@
+source /cvmfs/sft.cern.ch/lcg/views/LCG_96/x86_64-centos7-gcc8-opt/setup.sh 
+export LHAPDF_DATA_PATH=/cvmfs/sft.cern.ch/lcg/external/lhapdfsets/current/
+export LHAPDF_DATA_PATH=/cvmfs/sft.cern.ch/lcg/views/LCG_96/x86_64-centos7-gcc8-opt/share/LHAPDF/:$LHAPDF_DATA_PATH
+
