ed4915b2ae881ed6e559d7c69bca98bbec923d79
Pietro <pietro.govoni@cern.ch>
2020-07-08 18:01:10 +0200

    allowing to choose the output base folder

--- generation/submit.py
+++ generation/submit.py
@@ -23,8 +23,8 @@ def create_job_sub (resultfile, sourcefile, replace):
 
 if __name__ == "__main__":
 
-    if (len(sys.argv) == 1):
-        print 'usage: submit.py process_folder [events(10000)] [jobs(100)] [shell script(job.sh)]'
+    if (len(sys.argv) < 3):
+        print 'usage: submit.py process_folder_relative_path output_base_folder [events(10000)] [jobs(100)] [shell script(job.sh)]'
         sys.exit (1)
 
     process_folder = sys.argv[1]
@@ -33,7 +33,10 @@ if __name__ == "__main__":
         print ('the folder ' + process_folder + ' does not exist, exiting\n')
         sys.exit (1)
 
-    results_folder = os.getcwd () + '/' + process_folder + '_results'
+    process_name = sys.argv[1].split ('/')[-1]
+
+    results_folder = sys.argv[2] + '/' + process_name + '_results'
+
     if (os.path.exists(results_folder)):
         content = os.listdir (results_folder)
         if len (content) > 0:
@@ -43,16 +46,16 @@ if __name__ == "__main__":
         os.mkdir (results_folder)
 
     events_number  = '10000'
-    if (len (sys.argv) > 2):
-        events_number = sys.argv[2]
+    if (len (sys.argv) > 3):
+        events_number = sys.argv[3]
 
     jobs_number  = '100'
-    if (len (sys.argv) > 3):
-        jobs_number = sys.argv[3]
+    if (len (sys.argv) > 4):
+        jobs_number = sys.argv[4]
 
     shell_script_template   = 'job.sh'
-    if (len (sys.argv) > 4):
-        shell_script_template = sys.argv[4]
+    if (len (sys.argv) > 5):
+        shell_script_template = sys.argv[5]
     if (not os.path.exists(os.getcwd () + '/' + shell_script_template)):
         print ('the file ' + shell_script_template + ' does not exist, exiting\n')
         sys.exit (1)
@@ -61,13 +64,13 @@ if __name__ == "__main__":
     shutil.copy2 (shell_script_template, job_file)
 
     replace = [
-        ['PROCESS_NAME_CHANGEME' , process_folder ],
-        ['BASE_FOLDER_CHANGEME'  , os.getcwd ()   ],
-        ['EVENTS_NUMBER_CHANGEME', events_number  ],
-        ['JOBS_NUMBER_REPLACEME' , jobs_number    ],
-        ['EXECUTABLE_CHANGEME'   , job_file       ]
+        ['PROCESS_NAME_CHANGEME' , process_name  ],
+        ['BASE_FOLDER_CHANGEME'  , os.getcwd ()  ],
+        ['EVENTS_NUMBER_CHANGEME', events_number ],
+        ['JOBS_NUMBER_REPLACEME' , jobs_number   ],
+        ['EXECUTABLE_CHANGEME'   , job_file      ]
       ]
-    submit_file = results_folder + '/' + process_folder + '.sub'
+    submit_file = results_folder + '/' + process_name + '.sub'
     create_job_sub (submit_file, 'example.sub', replace)
 
     # submit the condor job
