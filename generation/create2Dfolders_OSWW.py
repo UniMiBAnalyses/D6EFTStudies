f063a25cc30805806c3b9ba14b37600365647064
Giacomo Boldrini <gboldrin@cern.ch>
2021-08-21 14:55:56 +0200

    Updated SMEFTsim

--- generation/create2Dfolders_OSWW.py
+++ generation/create2Dfolders_OSWW.py
@@ -13,8 +13,8 @@ import sys
 
 if __name__ == "__main__":
 
-#  switchOn = ['21','24','25','28','32','45','46','48','49','53','54','55','56','57','58']
-  switchOn = ['21', '32', '46', '48', '49', '54', '55', '56', '57', '58']
+  switchOn = ['21','24','25','28','32','45','46','48','49','53','54','55','56','57','58']
+  #switchOn = ['21', '32', '46', '48', '49', '54', '55', '56', '57', '58']
 
   proc_ID = 'OSWW'
 
@@ -116,7 +116,7 @@ if __name__ == "__main__":
       tag = sortedsel[i][1] + '_' + sortedsel[j][1]
 
       f_launchfile = open ('launch_' + proc_ID + '_' + tag + '_IN.txt', 'w')
-      f_launchfile.write ('import model SMEFTsim_A_U35_MwScheme_UFO_v3_1-' + tag + '_massless\n')
+      f_launchfile.write ('import model SMEFTsim_U35_MwScheme_UFO-' + tag + '_massless\n')
       f_launchfile.write ('generate p p > e+ ve mu- vm~ j j QCD=0  SMHLOOP=0 NP==1  NP' + sortedsel[i][1] + '^2==1   NP' + sortedsel[j][1] + '^2==1\n')
       f_launchfile.write ('add process p p > e- ve~ mu+ vm j j QCD=0  SMHLOOP=0 NP==1  NP' + sortedsel[i][1] + '^2==1   NP' + sortedsel[j][1] + '^2==1\n')
       f_launchfile.write ('output ' + proc_ID + '_' + tag + '_IN')
